161281ac23fec020e5a3dd8285ff1480de0dd033
AkashiNeko <akashineko@qq.com>
2024-06-28 14:33:43 +0800

    modify main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello world" << std::endl;
+    // modify main.cpp
+    std::cout << "Hello main branch" << std::endl;
+    // std::cout << "Hello world" << std::endl;
     return 0;
 }
 
