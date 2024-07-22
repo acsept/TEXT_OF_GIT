0196769f956018685cacd3d574d97dbc09e66ce6
WW <31331>
2024-07-22 13:59:24 +0800

    using namespace std

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+using namespace std;
+
+
 
 int main() {
 
     // modify main.cpp
-    std::cout << "Hello main branch" << std::endl;
+    //std::cout << "Hello main branch" << std::endl;
     // std::cout << "Hello world" << std::endl;
+    cout << "Hello main branch" << endl;
 
     std::cout <<"this main has be changed!"<<std::endl;
 
